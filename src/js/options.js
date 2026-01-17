10569d2a51bbed967e859ca23e39ba369f9c0d42
ito <t.ito@itreat.co.jp>
2026-01-17 21:22:10 +0900

    feat: ボタンアイコンを追加

--- src/js/options.js
+++ src/js/options.js
@@ -518,8 +518,14 @@ import '@melloware/coloris/dist/coloris.css';
             <div class="project-name">${escapeHtml(project.name)}</div>
           </div>
           <div class="project-actions">
-            <button class="btn btn-edit edit-project" data-project-id="${project.id}" data-i18n="projects.edit">Edit</button>
-            <button class="btn btn-danger delete-project" data-project-id="${project.id}" data-i18n="projects.delete">Delete</button>
+            <button class="btn btn-edit edit-project" data-project-id="${project.id}">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" class="btn-icon btn-icon-small"><path d="M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z"/><path d="m15 5 4 4"/></svg>
+              <span data-i18n="projects.edit">Edit</span>
+            </button>
+            <button class="btn btn-danger delete-project" data-project-id="${project.id}">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="btn-icon btn-icon-small"><path d="M10 11v6"/><path d="M14 11v6"/><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6"/><path d="M3 6h18"/><path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"/></svg>
+              <span data-i18n="projects.delete">Delete</span>
+            </button>
           </div>
         </div>
         <div class="project-domains-section">
